dc7d809cc02dd1f6d8fa72e9775f6156af127bb2
Tiago Rangel <70700766+tiagorangel2011@users.noreply.github.com>
2023-03-26 16:08:48 +0100

    Improve code, add comments

--- main.py
+++ main.py
@@ -6,19 +6,21 @@ from ai import ask
 r = sr.Recognizer()
 engine = pyttsx3.init()
 
+print("Listening...")
+
 async def answer(speech):
-    response = await ask(speech)
-    print(response.replace("\n", " ") + "\n")
-    engine.say(response)
+    response = await ask(speech).replace("\n", " ") # This calls the AI engine
+    print(response + "\n")
+    engine.say(response) # Speak the response
     engine.runAndWait()
 
 while True:
-    with sr.Microphone() as source:
+    with sr.Microphone() as source: # Grab mic
         r.adjust_for_ambient_noise(source)
         audio = r.listen(source)
 
         try:
-            # sometimes the engine thinks clippy means "creepy"
+            # This converts the speech to text. Sometimes the library thinks clippy means "creepy" so I need to replace it:).
             speech = r.recognize_google(audio).lower().replace("hi clippy", "hey clippy").replace("hello clippy", "hey clippy").replace("creepy", "clippy")
 
             if "hey clippy" in speech and speech != "":
@@ -28,4 +30,4 @@ while True:
                 engine.say("Hi")
 
         except sr.UnknownValueError:
-            pass
+            pass # Ignore errors.
